17e590a6c210a57578ec374792a32dfab2dcb7b1
RyanDBarnett <conner2468@gmail.com>
2019-04-15 11:02:56 -0600

    Create generateChoices function

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const Card = () => {
-  return <h3>Card</h3>;
+const Card = ({currentQuestion, showNextQuestion, resetAllQuestions, resetIncorrectQuestions}) => {
+  function generateChoices() {
+    return currentQuestion.choices.map((choice, index) => {
+      return <button key={index} onClick={showNextQuestion}>{choice}</button>
+    });
+  }
+  let answerOptions = currentQuestion ? 
+    generateChoices() :
+    <div>
+      <button onClick={resetAllQuestions}>Reset All Questions</button>
+      <button onClick={resetIncorrectQuestions}>Practice Incorrect Questions</button>
+    </div>;
+  let display = currentQuestion ? <h4>{currentQuestion.question}</h4> : <h4>All Done! Do you want to go again?</h4>
+  return (
+    <div className="card">
+      <h3>Card</h3>
+      {display}
+      {answerOptions}
+    </div>
+  )
 }
 
 export default Card;
