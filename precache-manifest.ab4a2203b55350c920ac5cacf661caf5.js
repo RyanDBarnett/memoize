84e3f255d9e9efe27f8278e7921be152c62a0f03
RyanDBarnett <conner2468@gmail.com>
2019-04-17 08:42:10 -0600

    Updates

---
+++ precache-manifest.ab4a2203b55350c920ac5cacf661caf5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "30e7b438150a0a71931ab2dac2cb3558",
+    "url": "/memoize/static/media/hot-air-balloon.30e7b438.png"
+  },
+  {
+    "revision": "921bd6f4ee5ded1e0866ddf8b120d484",
+    "url": "/memoize/static/media/hot-air-ballons-sky.921bd6f4.jpg"
+  },
+  {
+    "revision": "aeccfde9f522beb07f3b",
+    "url": "/memoize/static/js/runtime~main.9d38199b.js"
+  },
+  {
+    "revision": "4bbdf40b5e67617ab751",
+    "url": "/memoize/static/js/main.ac105f2d.chunk.js"
+  },
+  {
+    "revision": "0ed14e37a66cd65d2e82",
+    "url": "/memoize/static/js/2.676b93aa.chunk.js"
+  },
+  {
+    "revision": "4bbdf40b5e67617ab751",
+    "url": "/memoize/static/css/main.45b71b9d.chunk.css"
+  },
+  {
+    "revision": "0ed14e37a66cd65d2e82",
+    "url": "/memoize/static/css/2.ac827ad8.chunk.css"
+  },
+  {
+    "revision": "0eb545b3fec2fba93d6e9e267c527d75",
+    "url": "/memoize/index.html"
+  }
+];
